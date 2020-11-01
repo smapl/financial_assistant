c9c940025eb4959ff05a7ee0d7fc41ba1449b8c3
smapl <a.mustafin00@mail.ru>
2020-11-01 21:33:31 +0300

    hotfix

--- src/financial_assistant/views.py
+++ src/financial_assistant/views.py
@@ -1,31 +1,72 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import request, render_template, session, redirect, url_for
 from flask_cors import CORS, cross_origin
 
-from .create_app import create_app
+from loguru import logger
 
+
+from .create_app import create_app
+from .models import Users
+from .db_utils import PostgreConnect
 
 app = create_app()
-CORS(app)
+CORS(app=app)
+inside_data = PostgreConnect()
+connect = inside_data.output()
+
+user = Users(connect)
 
 
-@app.route("/", methods=["GET"])
+@app.route("/identification", methods=["GET"])
 @cross_origin()
-def begin():
+def begin_page():
     return render_template("index.html")
 
 
-@app.route("/main", methods=["POST"])
+@app.route("/personality", methods=["GET"])
 @cross_origin()
-def main():
-    resp = request.json.get("transp")
-    print(resp)
-    if resp == True:
-        return redirect(url_for("other"))
+def personality():
+    if "user_name" in session:
+        return render_template("main.html")
     else:
-        return "error"
+        return "Not login"
 
 
-@app.route("/other")
+@app.route("/identification/registration", methods=["POST", "GET"])
 @cross_origin()
-def other():
-    return render_template("main.html")
+def create_user():
+    if request.method == "GET":
+        return "this get resquest (registration)"
+
+    login = request.json.get("login")
+    password = request.json.get("password")
+    fname = request.json.get("fname")
+    lname = request.json.get("lname")
+    email = request.json.get("email")
+    date_of_birth = str(request.json.get("date_of_birth"))
+
+    if request.method == "POST":
+        result = user.create_user(login, password, fname, lname, date_of_birth, email)
+
+        return {"Response": result}
+
+    else:
+        return "This is Get request"
+
+
+@app.route("/identification/login", methods=["POST", "GET"])
+@cross_origin()
+def check_user():
+    if request.method == "GET":
+        return "this get resquest (login)"
+
+    if request.method == "POST":
+        login = request.json.get("login")
+        password = request.json.get("password")
+        db_res = user.check_user(login, password)
+
+        logger.info(db_res["result"])
+        if db_res["result"] == True:
+            session["user_name"] = login
+            logger.info(session["user_name"])
+
+        return db_res
