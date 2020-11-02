57d9be3f85325942af43a6d63c14b9f34518e3ff
smapl <a.mustafin00@mail.ru>
2020-11-02 03:30:39 +0300

    create redirect

--- src/financial_assistant/static/scripts/index.js
+++ src/financial_assistant/static/scripts/index.js
@@ -1,37 +1,7 @@
-function registrtion() {
-  let resg_url = "http://0.0.0.0:5000/identification/registration";
-
-  let login = document.querySelector(".login").value;
-  let password = document.querySelector(".password").value;
-  let fname = document.querySelector(".fname").value;
-  let lname = document.querySelector(".lname").value;
-  let email = document.querySelector(".email").value;
-  let date_of_birth = document.querySelector(".date_of_birth").value;
-
-  let data_to_server = {
-    login: login,
-    password: password,
-    fname: fname,
-    lname: lname,
-    date_of_birth: date_of_birth,
-    email: email,
-  };
-  console.log(data_to_server);
-  axios.post(resg_url, data_to_server).then(function (response) {
+function transport() {
+  data = { module: true };
+  axios.post("http://0.0.0.0:5000/handler", data).then((response) => {
     console.log(response.data);
-  });
-}
-
-function login() {
-  let log_url = "http://0.0.0.0:5000/identification/login";
-  let check_login = document.querySelector(".check_login").value;
-  let check_password = document.querySelector(".check_password").value;
-  let data_to_check = { login: check_login, password: check_password };
-  console.log(data_to_check);
-
-  axios.post(log_url, data_to_check).then((response) => {
-    if (response.data["result"] == true) {
-      window.location.href = "http://0.0.0.0:5000/personality";
-    }
+    window.location.href = response.data;
   });
 }
--- src/financial_assistant/views.py
+++ src/financial_assistant/views.py
@@ -1,72 +1,28 @@
-from flask import request, render_template, session, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_cors import CORS, cross_origin
 
-from loguru import logger
-
-
 from .create_app import create_app
-from .models import Users
-from .db_utils import PostgreConnect
 
 app = create_app()
-CORS(app=app)
-inside_data = PostgreConnect()
-connect = inside_data.output()
-
-user = Users(connect)
+CORS(app)
 
 
-@app.route("/identification", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 @cross_origin()
-def begin_page():
-    return render_template("index.html")
+def start_page():
+    return render_template("start.html")
 
 
-@app.route("/personality", methods=["GET"])
+@app.route("/page_one", methods=["GET"])
 @cross_origin()
-def personality():
-    if "user_name" in session:
-        return render_template("main.html")
-    else:
-        return "Not login"
+def page_one():
+    return render_template("page.html")
 
 
-@app.route("/identification/registration", methods=["POST", "GET"])
+@app.route("/handler", methods=["POST"])
 @cross_origin()
-def create_user():
-    if request.method == "GET":
-        return "this get resquest (registration)"
-
-    login = request.json.get("login")
-    password = request.json.get("password")
-    fname = request.json.get("fname")
-    lname = request.json.get("lname")
-    email = request.json.get("email")
-    date_of_birth = str(request.json.get("date_of_birth"))
-
-    if request.method == "POST":
-        result = user.create_user(login, password, fname, lname, date_of_birth, email)
-
-        return {"Response": result}
-
-    else:
-        return "This is Get request"
-
-
-@app.route("/identification/login", methods=["POST", "GET"])
-@cross_origin()
-def check_user():
-    if request.method == "GET":
-        return "this get resquest (login)"
-
-    if request.method == "POST":
-        login = request.json.get("login")
-        password = request.json.get("password")
-        db_res = user.check_user(login, password)
-
-        logger.info(db_res["result"])
-        if db_res["result"] == True:
-            session["user_name"] = login
-            logger.info(session["user_name"])
-
-        return db_res
+def handler():
+    print(request.json)
+    if request.json.get("module") == True:
+        return "http://0.0.0.0:5000/page_one"
+    return "error"
