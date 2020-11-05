ddb100ca8dc62ddfa8159d13b5a9ce64fd9d9981
smapl <a.mustafin00@mail.ru>
2020-11-05 20:01:16 +0300

    it os workgit add .git add .git add .git add .

--- src/financial_assistant/static/scripts/index.js
+++ src/financial_assistant/static/scripts/index.js
@@ -31,31 +31,28 @@ function login() {
     password: check_password,
   };
 
-  let res = send_req(log_url, data_to_check);
-
-  if ((res != false) & (res != undefined)) {
-    if (res["result"] == true) {
-      console.log(res["redirect_url"]);
-      window.location.href = res["redirect_url"];
-    } else {
-      console.log(res["result"]);
-    }
-  } else {
-    console.log(res);
-  }
-}
-
-function send_req(req_url, params) {
   axios
-    .post(req_url, params, { headers: { "content-type": "application/json" } })
+    .post(log_url, data_to_check, {
+      headers: { "content-type": "application/json" },
+    })
     .then((response) => {
       if (response.status == 200) {
-        return response.data;
-      } else {
-        return false;
+        res = response.data;
+        if (res["result"] == true) {
+          console.log(res["redirect_url"]);
+          window.location.href = res["redirect_url"];
+        } else {
+          console.log(res["result"]);
+        }
       }
     })
     .catch((error) => {
       console.log(error);
     });
 }
+
+let reg = document.getElementById("reg");
+let log = document.getElementById("log");
+console.log(reg, log);
+reg.onclick = registrtion;
+log.onclick = login;
